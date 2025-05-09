4986b5f5426eb034873863443e58db9375c31d04
mikerabs <mrabayda4@icloud.com>
2025-05-09 14:15:05 +0000

    working after adding admin user

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # DataPipeline
 a Real-Time Data Pipeline made with Apache Airflow, Kafka, Spark, and Cassandra
+
+## Command to create admin user within airflow ui
+docker exec -it datapipeline-webserver-1 airflow users create \
+    --username admin \
+    --firstname Admin \
+    --lastname Admin \
+    --role Admin \
+    --email admin@example.com \
+    --password admin
+
+## Other things that had to be fixed:
+Had to add firewall rules to port 8080 and port 9021 for airflow ui and confluent respectively
+
+had to remove health checks on containers/make the start time delay 100+ seconds due to slow boot up
--- script/entrypoint.sh
+++ script/entrypoint.sh
@@ -1,12 +1,27 @@
 #!/bin/bash
+#set -e
+#if [ -e "/opt/airflow/requirements.txt" ]; then
+  #$(command -v pip) install --user -r /opt/airflow/requirements.txt
+#fi
+#if [ ! -f "/opt/airflow/airflow.db" ]; then
+  #airflow db init && \
+  #airflow users create --username admin --firstname admin --lastname admin --role Admin --email admin@example.com --password admin
+#fi
+#airflow db upgrade
+#exec airflow webserver
+
 set -e
-if [ -e "/opt/airflow/requirements.txt" ]; then
-  $(command -v pip) install --user -r requirements.txt
+
+cd /opt/airflow
+
+# Install any dependencies
+if [ -f "/opt/airflow/requirements.txt" ]; then
+  pip install --user -r /opt/airflow/requirements.txt
 fi
-if [ ! -f "/opt/airflow/airflow.db" ]; then
-  airflow db init && \
-  airflow users create --username admin --firstname admin --lastname admin --role Admin --email admin@example.com --password admin
-fi
+
+# Upgrade DB
 airflow db upgrade
-exec airflow webserver
+
+# Pass control to the command in docker-compose (e.g., webserver or scheduler)
+exec "$@"
 
